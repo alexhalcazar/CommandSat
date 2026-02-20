be65f9567c0546fef2d37cf28b07e32f6e902a83
alexhalcazar <alexhalcazar369@gmail.com>
2026-02-19 20:53:02 -0800

    refactor: move ESLint config and dependencies to root

--- eslint.config.js
+++ eslint.config.js
@@ -6,7 +6,7 @@ import { defineConfig, globalIgnores } from 'eslint/config';
 import eslintConfigPrettier from 'eslint-config-prettier/flat';
 
 export default defineConfig([
-    globalIgnores(['dist', 'public/cesium']),
+    globalIgnores(['frontend/dist', 'frontend/public/cesium']),
     {
         files: ['**/*.{js,jsx}'],
         extends: [
