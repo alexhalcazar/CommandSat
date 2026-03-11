41cbc46ea04a27cb3c84e8d757bf8a24205916dd
alexhalcazar <alexhalcazar369@gmail.com>
2026-03-11 12:05:58 -0700

    docs: update JSDoc typedefs for request params and response objects

--- frontend/src/components/EntityPoint.jsx
+++ frontend/src/components/EntityPoint.jsx
@@ -4,16 +4,18 @@ import { Cartesian3 } from 'cesium';
 /**
  * A resuable Resium component that renders a Cesium Entity with PointGraphics
  * at a fixed geograohic location
- * @param {Object} props
- * @param {number} props.longitude - The longitude, in degrees
- * @param {number} props.latitude - The latitude, in degrees
- * @param {number} props.height - The height, in meters, above the ellipsoid
- * @param {number} props.pixelSize - specifies the size in pixels
- * @returns {JSX.Element} - A Resium Entity configured with PointGraphics
+ * @typedef {Object} EntityPoint
+ * @property {number} longitude - The longitude, in degrees
+ * @property {number} latitude - The latitude, in degrees
+ * @property {number} height - The height, in meters, above the ellipsoid
+ * @property {number} pixelSize - specifies the size in pixels
  */
 
-export const EntityPoint = (entityPoint) => {
-    const { longitude, latitude, height, pixelSize } = entityPoint;
+/**
+ * @param {EntityPoint} entityPoint
+ * @returns {JSX.Element} - A Resium Entity configured with PointGraphics
+ */
+export const EntityPoint = ({ longitude, latitude, height, pixelSize }) => {
     const position = Cartesian3.fromDegrees(longitude, latitude, height);
 
     return (
