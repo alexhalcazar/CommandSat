ac79bf32c9825fe0df77fa36320d79365ac20656
alexhalcazar <alexhalcazar369@gmail.com>
2026-05-11 14:40:20 -0700

    feat: create Login/Register pages with JWT authentication

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,11 +1,16 @@
 import { Dashboard } from '@pages/Dashboard/Dashboard';
+import { Register } from './pages/Register/Register.jsx';
+import { Login } from './pages/Login/Login.jsx';
+import { Routes, Route } from 'react-router-dom';
 
 function App() {
     return (
         <>
-            <div>
-                <Dashboard />
-            </div>
+            <Routes>
+                <Route path='/register' element={<Register />} />
+                <Route path='/login' element={<Login />} />
+                <Route path='/dashboard' element={<Dashboard />} />
+            </Routes>
         </>
     );
 }
